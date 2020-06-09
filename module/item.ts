ddee08e646b3429aa01bc5048d9d5206eb135f8f
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-06-08 18:04:42 -0600

    Initial BITS Implementation
    Adds instincts and traits to character sheet to go with the beliefs
    on the sheet.

--- module/item.ts
+++ module/item.ts
@@ -1,5 +1,16 @@
+import { Trait } from "./items/trait.js";
+
 export class BWItem extends Item {
     prepareData() {
         super.prepareData();
+        if (this.type === "trait") {
+            Trait.prototype.prepareData.bind(this)();
+        }
     }
+}
+
+export interface ArthaEarner {
+    fate: boolean;
+    persona: boolean;
+    deeds: boolean;
 }
---
+++ module/items/trait-sheet.ts
@@ -0,0 +1,25 @@
+import { Trait } from "./trait.js";
+
+export class TraitSheet extends ItemSheet {
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {});
+    }
+
+    get template() {
+        return "systems/burningwheel/templates/items/trait.html";
+    }
+
+    getData() {
+        const data = super.getData() as TraitSheetData;
+        data.traitTypes = {
+            character: "Character",
+            "call-on": "Call-on",
+            die: "Die"
+        }
+        return data;
+    }
+}
+
+interface TraitSheetData extends ItemSheetData {
+    traitTypes: { [index: string]: string }
+}

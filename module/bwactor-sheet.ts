633ed4403aab921b073a22a21c64e8688a7414ea
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-06-11 20:40:08 -0600

    Add test tracking to stats
    Adds the ability to track tests for stats.
    Adds workaround for radio buttons not properly saving
    Adds helper related to radio buttons not being properly checked

--- module/bwactor-sheet.ts
+++ module/bwactor-sheet.ts
@@ -32,4 +32,32 @@ export class BWActorSheet extends ActorSheet {
         updateParams[binding] = value;
         item.update(updateParams, {});
     }
+
+    _getFormData(form: HTMLFormElement) {
+        // fixes an issue with radio buttons all storing their values at the same
+        // time inside an array for a given set of radio button options.
+        // TODO check if this is needed after version 0.7
+        const fd = super._getFormData(form);
+        const dataValues = {};
+
+        for( const element of Object.values(form.elements)) {
+            // if an input is a radio input, we need to make sure to pick the right value for its'
+            // related property.
+            if (element instanceof HTMLInputElement && element.type === "radio") {
+                if (!dataValues[element.name]) {
+                    dataValues[element.name] = [];
+                }
+                // specifically, the value assigned to the currently checked element
+                if (element.checked) {
+                    dataValues[element.name].push(element.value);
+                }
+            }
+        }
+
+        // update the data model values to match the radio buttons
+        for( const k of Object.keys(dataValues)) {
+            fd.set(k, JSON.stringify(dataValues[k]));
+        }
+        return fd;
+    }
 }

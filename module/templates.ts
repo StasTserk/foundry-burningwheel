7b48035a2c59fcbd93bf0a9edd95b79ee1047e91
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-06-24 17:15:15 -0600

    Training Section Rough-in
    Initial pass on skill training
    Adds training checkbox to skill sheet
    Adds training Section
    Adds automatic training aptitude calculation

--- module/templates.ts
+++ module/templates.ts
@@ -4,6 +4,7 @@ export async function preloadHandlebarsTemplates() {
     // Define template paths to load
     const templatePaths = [
       // Actor Sheet Partials
+      "systems/burningwheel/templates/parts/learning.html",
       "systems/burningwheel/templates/parts/ptgs.html",
       "systems/burningwheel/templates/parts/rollable-item.html",
       "systems/burningwheel/templates/parts/rollable-skill.html"

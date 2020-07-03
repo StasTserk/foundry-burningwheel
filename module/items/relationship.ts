282ce336aa41f4a8756b8a10fe030a6728d95a2c
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-07-03 17:57:32 -0600

    Style traits, rollables, relationships, weapons
    Styles the rest of the sheet for the initial style pass

--- module/items/relationship.ts
+++ module/items/relationship.ts
@@ -1,9 +1,18 @@
 import { BWActor } from "../actor.js";
+import { DisplayClass } from "./item.js";
 
 export class Relationship extends Item {
     prepareData() {
         this.data.data.safeId = this._id;
         this.data.data.aptitude = parseInt(this.actor.data.data.circles.exp, 10) || 0;
+
+        if (this.data.data.hateful || this.data.data.enmity) {
+            this.data.data.cssClass = "relationship-hostile";
+        } else if (this.data.data.romantic || this.data.data.immediateFamily) {
+            this.data.data.cssClass = "relationship-friendly";
+        } else {
+            this.data.data.cssClass = "relationship-neutral";
+        }
     }
 
     get actor(): BWActor {
@@ -17,13 +26,14 @@ interface RelationshipDataRoot extends BaseEntityData {
     data: RelationshipData;
 }
 
-export interface RelationshipData {
+export interface RelationshipData extends DisplayClass {
     description: string;
     forbidden: boolean;
     immediateFamily: boolean;
     otherFamily: boolean;
     romantic: boolean;
     hateful: boolean;
+    enmity: boolean;
     influence: string;
     building: string;
     buildingProgress: number;

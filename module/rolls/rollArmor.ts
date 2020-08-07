fb2b0981ce3336816f893b5c73a8ebcbb3cbc053
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-08-07 14:22:46 -0600

    Add ability to edit armor damage
    Change the entry displaying the armor damage to be an editable input
    field linked to the individual armor location's damage.

--- module/rolls/rollArmor.ts
+++ module/rolls/rollArmor.ts
@@ -8,7 +8,7 @@ export function handleArmorRoll(target: HTMLButtonElement, sheet: BWActorSheet):
     const armorItem = actor.getOwnedItem(armorId) as Armor;
     const location = target.dataset.location || "";
     const dice = parseInt(armorItem.data.data.dice);
-    const damage = parseInt(armorItem.data.data[`damage${location.titleCase()}`]);
+    const damage = parseInt(armorItem.data.data[`damage${location.charAt(0).toUpperCase() + location.slice(1)}`]);
     const numDice = dice - damage;
     return rollDice(numDice, false, "B");
 }

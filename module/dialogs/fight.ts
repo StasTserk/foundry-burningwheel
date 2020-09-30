23fd9aa323eda075a7ced9f147fe07e6dc260d5b
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-09-30 15:24:31 -0600

    Add implementation for fight actions, more
    Fight actions can now be scripted
    The GM can show and hide actions for characters he does not want to
    control -- Players only see actions of characters they own.
    Data is synchronized across all clients.

--- module/dialogs/fight.ts
+++ module/dialogs/fight.ts
@@ -2,18 +2,26 @@ import { BWItemData } from "../items/item.js";
 import { NpcDataRoot } from "../npc.js";
 import { BWActor, CharacterDataRoot } from "../bwactor.js";
 import { StringIndexedObject } from "../helpers.js";
+import { ExtendedTestData, ExtendedTestDialog } from "./extendedTestDialog.js";
 
-export class FightDialog extends Dialog {
+export class FightDialog extends ExtendedTestDialog<FightDialogData> {
     constructor(d: DialogData, o?: ApplicationOptions) {
         super(d, o);
         this.data.data.participants = this.data.data.participants || [];
         this.data.data.participantIds = this.data.data.participantIds || [];
-        this.data.actors = []; // replace this with actor lookups later -- this.data.data.participantIds
         this.data.actionOptions = options;
+        this.data.actors = [];
+        this.data.topic = "Fight";
+        this.data.settingName = "fight-data";
     }
 
-    getData(): unknown {
-        const data = Object.assign(super.getData(), this.data.data ) as FightDialogData;
+    getData(): FightDialogData {
+        const data = super.getData();
+
+        if (!this.data.actors.length) {
+            this.data.actors = game.actors.filter((i: BWActor) => this.data.data.participantIds.includes(i.id)) || [];
+        }
+
         const actors = game.actors.entities;
         data.gmView = game.user.isGM;
         data.participantOptions = actors
@@ -24,9 +32,21 @@ export class FightDialog extends Dialog {
 
         data.participants.forEach(p => {
             const actor = this.data.actors.find(a => a.id === p.id) as BWActor;
+            if (!actor) { return; }
             p.weapons = actor.data.fightWeapons.map(s => {
-                return { id: (s as BWItemData & { id:string }).id, label: s.name };
+                return { id: (s as BWItemData & { _id:string })._id, label: s.name };
             });
+
+            p.showAction2 = !!p.action1;
+            p.showAction3 = !!p.action2;
+
+            p.showAction5 = !!p.action4;
+            p.showAction6 = !!p.action5;
+
+            p.showAction8 = !!p.action7;
+            p.showAction9 = !!p.action8;
+
+            p.showActions = (data.gmView && !p.gmHidden) || (!data.gmView && this.data.actors.find(a => a.id === p.id)?.owner);
         });
         data.actionOptions = this.data.actionOptions;
         return data;
@@ -36,12 +56,30 @@ export class FightDialog extends Dialog {
         super.activateListeners(html);
         html.on('submit', (e) => { e.preventDefault(); });
         html.find('select[name="newParticipant"]').on('change', (e: JQuery.ChangeEvent) => this._addNewParticipant(e.target));
+        html.find('button[data-action="removeFighter"]').on('click', (e: JQuery.ClickEvent) => this._removeParticipant(e.target));
         html.find('div[data-action="toggleHidden"').on('click', (e: JQuery.ClickEvent) => this._toggleHidden(e.target));
+        html.find('.fighters-grid input, .fighters-grid select').on('change', (e: JQuery.ChangeEvent) => this._updateGridData(e));
     }
+
+    private _updateGridData(e: JQuery.ChangeEvent): void {
+        const index = parseInt(e.target.dataset.index || "0");
+        const newValue = e.target.type ==="checkbox" ? e.target.checked : e.target.value;
+        const dataPath = e.target.name;
+
+        this.data.data.participants[index][dataPath] = newValue;
+
+        this.persistState(this.data.data);
+        this.syncData(this.data.data);
+        this.render();
+    }
+    
     private _toggleHidden(target: HTMLDivElement): void {
+        if (!game.user.isGM) { return; }
         const index = parseInt(target.dataset.index || "0") ;
-        const hidden = this.data.data.participants[index].showActions;
-        this.data.data.participants[index].showActions = !hidden;
+        const hidden = this.data.data.participants[index].gmHidden;
+        this.data.data.participants[index].gmHidden = !hidden;
+        this.persistState(this.data.data);
+        this.syncData(this.data.data);
         this.render(true);
     }
     private _addNewParticipant(target: HTMLSelectElement): void {
@@ -50,13 +88,41 @@ export class FightDialog extends Dialog {
         this.data.actors.push(actor);
         this.data.data.participants.push({ ...toParticipantData(actor),
             action1: '', action2: '', action3: '', action4: '', action5: '',
-            action6: '', action7: '', action8: '', action9: '', showActions: true
+            action6: '', action7: '', action8: '', action9: '', gmHidden: false,
+            engagementBonus: 0, positionPenalty: 0
         } as ParticipantEntry);
+        this.data.data.participantIds.push(id);
+        this.persistState(this.data.data);
+        this.syncData(this.data.data);
+        this._syncActors();
         this.render();
     }
 
-    data: {
-        data: FightDialogData;
+    private _removeParticipant(target: HTMLElement): void {
+        const index = parseInt(target.dataset.index || "0");
+        this.data.data.participantIds.splice(index, 1);
+        this.data.data.participants.splice(index, 1);
+        this.persistState(this.data.data);
+        this.syncData(this.data.data);
+        this._syncActors();
+        this.render();
+    }
+
+    activateSocketListeners(): void {
+        super.activateSocketListeners();
+        game.socket.on("system.burningwheel", ({type}) => {
+            if (type === `syncActors${this.data.topic}`) {
+                this.data.actors = game.actors.filter((i: BWActor) => this.data.data.participantIds.includes(i.id)) || [];
+                this.render();
+            }
+        });
+    }
+
+    private _syncActors() {
+        game.socket.emit("system.burningwheel", { type: `syncActors${this.data.topic}`});
+    }
+
+    data: ExtendedTestData<FightDialogData> & {
         actionOptions: StringIndexedObject<string[]>;
         actors: BWActor[];
     };
@@ -74,21 +140,6 @@ export class FightDialog extends Dialog {
         }, { overwrite: true });
     }
 
-    _getHeaderButtons(): { label: string, icon: string, class: string, onclick: (e: JQuery.ClickEvent) => void; }[] {
-        let buttons = super._getHeaderButtons();
-        if (game.user.isGM) {
-            buttons = [{
-                label: "Show",
-                icon: "fas fa-eye",
-                class: "force-show-fight",
-                onclick: (_) => {
-                    game.socket.emit("system.burningwheel", { type: "showFight" });
-                }
-            }].concat(buttons);
-        }
-        return buttons;
-    }
-
     static addSidebarControl(html: JQuery): void {
         const buttonElement = document.createElement("button");
         buttonElement.textContent = "Fight";
@@ -96,36 +147,6 @@ export class FightDialog extends Dialog {
         buttonElement.onclick = () => game.burningwheel.fight.render(true);
         const combatHeader = $(html).find("header");
         combatHeader.prepend(buttonElement);
-    }
-
-    // private _propagateChange(e: JQuery.ChangeEvent): void {
-    //     const newValue = e.target.type ==="checkbox" ? e.target.checked : e.target.value;
-    //     const dataPath = e.target.name;
-        
-    //     const data = {};
-    //     data[dataPath] = newValue;
-
-    //     mergeObject(this.data.data, data);
-    //     if (game.user.isGM) {
-    //         game.settings.set("burningwheel", "fight-data", JSON.stringify(this.data.data));
-    //     }
-    //     game.socket.emit("system.burningwheel", { type: "updateFight", data });
-    //     this.render(true);
-    //     e.target.focus();
-    // }
-
-    activateSocketListeners(): void {
-        game.socket.on("system.burningwheel", ({type, data}) => {
-            if (type === "updateFight") {
-                mergeObject(this.data.data, data);
-                if (game.user.isGM) {
-                    game.settings.set("burningwheel", "fight-data", JSON.stringify(this.data.data));
-                }
-                if (this.rendered) { this.render(true); }
-            } else if (type === "showFight") {
-                this.render(true);
-            }
-        });
     }
 }
 
@@ -151,10 +172,24 @@ export interface FightDialogData {
 }
 
 interface ParticipantEntry {
+    showAction3: boolean;
+    showAction2: boolean;
+    showAction5: boolean;
+    showAction6: boolean;
+    showAction8: boolean;
+    showAction9: boolean;
+
     id: string;
     name: string;
     reflexes: string;
-    showActions: boolean;
+
+    gmHidden: boolean;
+    showActions?: boolean;
+    
+    weaponId: string;
+    engagementBonus: number;
+    positionPenalty: number;
+
     imgSrc: string;
     action1: string;
     action2: string;

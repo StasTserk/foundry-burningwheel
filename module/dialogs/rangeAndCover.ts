8c01c22e37e0a596a02f62427cfe729834be6330
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-11-13 15:06:23 -0700

    Range and Cover - templates
    Adds the ability to pick teams, some initial styling for the range and
    cover dialog.

--- module/dialogs/rangeAndCover.ts
+++ module/dialogs/rangeAndCover.ts
@@ -1,12 +1,15 @@
+import { BWActor } from "../bwactor.js";
 import { ExtendedTestData, ExtendedTestDialog } from "./extendedTestDialog.js";
 
-export class RandAndCoverDialog extends ExtendedTestDialog<RangeAndCoverData> {
+export class RangeAndCoverDialog extends ExtendedTestDialog<RangeAndCoverData> {
     constructor(d: DialogData, o?: ApplicationOptions) {
         super(d, o);
         this.data.topic = "range-and-cover";
         this.data.settingName = "rnc-data";
+        this.data.data.memberIds = this.data.data.memberIds || [];
+        this.data.data.teams = this.data.data.teams || [];
     }
-    data: ExtendedTestData<RangeAndCoverData>;
+    data: RangeAndCoverDialogData;
 
     get template(): string {
         return "systems/burningwheel/templates/dialogs/range-and-cover.hbs";
@@ -14,6 +17,73 @@ export class RandAndCoverDialog extends ExtendedTestDialog<RangeAndCoverData> {
 
     activateSocketListeners(): void {
         super.activateSocketListeners();
+    }
+
+    activateListeners(html: JQuery): void {
+        super.activateListeners(html);
+        html.find('input[name="showV1"], input[name="showV2"], input[name="showV3"]').on('change', (e: JQuery.ChangeEvent) => this.propagateChange(e));
+        html.find('select[name="newTeam"]').on('change', (e: JQuery.ChangeEvent) => this._addNewTeam(e.target));
+        html.find('select[name="newMember"]').on('change', (e: JQuery.ChangeEvent) => this._addNewMember(e.target));
+        html.find('*[data-action="delete-member"]').on('click', (e: JQuery.ClickEvent) => this._deleteMember(e.target));
+        html.find('*[data-action="toggle-hidden"]').on('click', (e: JQuery.ClickEvent) => this._toggleHidden(e.target));
+    }
+    
+    private _toggleHidden(target: HTMLElement): void {
+        const index = parseInt(target.dataset.index || "0");
+        const team = this.data.data.teams[index];
+
+        team.hideActions = !team.hideActions;
+
+        this.persistState(this.data.data);
+        this.syncData(this.data.data);
+        this.render();
+    }
+
+    private _addNewTeam(target: HTMLSelectElement): void {
+        const id = target.value;
+        const actor = this.data.actors.find(a => a.id === id) as BWActor;
+        this.data.data.teams.push({
+            members: [{ id, name: actor.name } ],
+            range: "Optimal",
+            hideActions: false
+        });
+        if (actor.data.type === "character") {
+            // ensure only one character can be added at once.
+            // reusing npcs is probably fine.
+            this.data.data.memberIds.push(id);
+        }
+        this.persistState(this.data.data);
+        this.syncData(this.data.data);
+        this.render();
+    }
+
+    private _addNewMember(target: HTMLSelectElement): void {
+        const id = target.value;
+        const index = parseInt(target.dataset.index || "0");
+        const team = this.data.data.teams[index];
+        const actor = this.data.actors.find(a => a.id === id) as BWActor;
+        
+        team.members.push({ id: actor.id, name: actor.name});
+        if (actor.data.type === "character") {
+            this.data.data.memberIds.push(id);
+        }
+
+        this.persistState(this.data.data);
+        this.syncData(this.data.data);
+        this.render();
+    }
+
+    private _deleteMember(target: HTMLElement): void {
+        const teamIndex = parseInt(target.dataset.index || "0");
+        const memberIndex = parseInt(target.dataset.memberIndex || "0");
+        const team = this.data.data.teams[teamIndex];
+        team.members.splice(memberIndex, 1);
+        if (team.members.length === 0) {
+            this.data.data.teams.splice(teamIndex, 1);
+        }
+        this.persistState(this.data.data);
+        this.syncData(this.data.data);
+        this.render();
     }
 
     static get defaultOptions(): FormApplicationOptions {
@@ -33,9 +103,49 @@ export class RandAndCoverDialog extends ExtendedTestDialog<RangeAndCoverData> {
         const combatHeader = $(html).find("header");
         combatHeader.prepend(buttonElement);
     }
+
+    getData(): RangeAndCoverData {
+        const data = super.getData() as RangeAndCoverData;
+        data.actionOptions = options;
+        if (!this.data.actors) {
+            this.data.actors = game.actors.entities as BWActor[];
+        }
+        data.actors = this.data.actors;
+        data.gmView = game.user.isGM;
+        return data;
+    }
+}
+
+interface RangeAndCoverDialogData extends ExtendedTestData<RangeAndCoverData> {
+    actors: BWActor[];
 }
 
 interface RangeAndCoverData {
-    teams: unknown[];
+    actors: BWActor[];
+    actionOptions: { [k:string]: string[] };
+    teams: RnCTeam[];
+    gmView: boolean;
+    memberIds: string[];
 }
 
+interface RnCTeam {
+    range: "Optimal" | "Extreme" | "Out of Range";
+    members: { name: string, id: string }[];
+    hideActions: boolean;
+    editable?: boolean;
+}
+
+const options = {
+    "Move In": [
+        "Close", "Sneak In", "Flank", "Charge"
+    ],
+    "Hold Ground": [
+        "Maintain Distance", "Hold Position"
+    ],
+    "Move Out": [
+        "Withdraw", "Sneak Out", "Fall Back", "Retreat"
+    ],
+    "Hesitation Actions": [
+        "Fall Prone", "Run Screaming", "Stand & Drool", "Swoon"
+    ]
+};

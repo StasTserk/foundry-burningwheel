79ae7f77a60eccffe1bdcf934f5df13698e7746c
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-11-20 14:04:13 -0700

    Initial special modifier UI work.

--- module/dialogs/difficultyDialog.ts
+++ module/dialogs/difficultyDialog.ts
@@ -1,7 +1,11 @@
 export class DifficultyDialog extends Application {
     difficulty: number;
     editable: boolean;
-    constructor(defaultDifficulty: number) {
+    splitPool: boolean;
+    customDiff: boolean;
+    noTrack: boolean;
+    mods: {name: string, amount: number }[];
+    constructor(defaultDifficulty: number, mods?: {name: string, amount: number}[] ) {
         super({
             template: "systems/burningwheel/templates/dialogs/gm-difficulty.hbs",
             classes: ["gm-difficulty"],
@@ -9,6 +13,9 @@ export class DifficultyDialog extends Application {
         });
         this.difficulty = defaultDifficulty;
         this.editable = game.user.isGM;
+
+        this.splitPool = this.customDiff = this.noTrack = false;
+        this.mods = mods || [];
     }
 
     activateListeners(html: JQuery): void {
@@ -18,6 +25,24 @@ export class DifficultyDialog extends Application {
             this.difficulty = difficulty;
             game.settings.set("burningwheel", "gmDifficulty", difficulty);
             game.socket.emit("system.burningwheel", { type: "difficulty", difficulty });
+        });
+        
+        html.find("#gm-diff-sp").on('change', e => {
+            this.splitPool = $(e.target).prop("checked") as boolean;
+        });
+        html.find("#gm-diff-custom").on('change', e => {
+            this.customDiff = $(e.target).prop("checked") as boolean;
+        });
+        html.find("#gm-diff-track").on('change', e => {
+            this.noTrack = $(e.target).prop("checked") as boolean;
+        });
+
+        html.find('input[name="newMod"]').on('change', e => {
+            const target = $(e.target);
+            const name = target.val() as string;
+            target.val("");
+            this.mods.push({ name, amount: 0});
+            this.render();
         });
 
         game.socket.on("system.burningwheel", ({ type, difficulty }) => {
@@ -32,11 +57,19 @@ export class DifficultyDialog extends Application {
         const data = super.getData() as DifficultyDialogData;
         data.difficulty = this.difficulty;
         data.editable = this.editable;
+        data.splitPool = this.splitPool;
+        data.noTrack = this.noTrack;
+        data.customDiff = this.customDiff;
+        data.modifiers = this.mods;
         return data;
     }
 }
 
 interface DifficultyDialogData {
+    modifiers: { name: string; amount: number; }[];
+    customDiff: boolean;
+    noTrack: boolean;
+    splitPool: boolean;
     difficulty: number;
     editable: boolean;
 }

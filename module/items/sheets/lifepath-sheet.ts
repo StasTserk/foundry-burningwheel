ce62b4e34d117794d44bbb5925849fb09750fc1f
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-11-27 13:28:56 -0800

    Add some drag and drop support for lifepaths
    Lifepaths (sort of) accept dropped skills and traits into themselves.

--- module/items/sheets/lifepath-sheet.ts
+++ module/items/sheets/lifepath-sheet.ts
@@ -1,3 +1,6 @@
+import { BWActor } from "../../actors/bwactor.js";
+import { DragData } from "../../helpers.js";
+import { BWItem } from "../item.js";
 import { BWItemSheet, BWItemSheetData } from "./bwItemSheet.js";
 
 export class LifepathSheet extends BWItemSheet {
@@ -8,5 +11,43 @@ export class LifepathSheet extends BWItemSheet {
 
     get template(): string {
         return "systems/burningwheel/templates/items/lifepath.hbs";
+    }
+
+    activateListeners(html: JQuery): void {
+        super.activateListeners(html);
+        html.on('drop', (e) => this._onDrop(e.originalEvent as DragEvent));
+    }
+
+    
+    protected async _onDrop(event: DragEvent): Promise<void> {
+        super._onDrop(event);
+        let data: DragData;
+        try {
+            data = JSON.parse(event.dataTransfer?.getData('text/plain') || "");
+        }
+        catch (err) {
+            console.log(err);
+            return;
+        }
+        if (data.type === "Item" && data.id) {
+            let item: BWItem | undefined;
+            if (data.pack) {
+                item = await (game.packs.find(p => p.collection === data.pack) as Compendium).getEntity(data.id) as BWItem;
+            } else if (data.actorId) {
+                item = (game.actors.find((a: BWActor) => a._id === data.actorId) as BWActor).getOwnedItem(data.id) as BWItem;
+            } else {
+                item = game.items.find((i: BWItem) => i.id === data.id) as BWItem;
+            }
+
+            if (item) {
+                if (item.type === "skill") {
+                    const skillList = `${this.item.data.data.skillList}${this.item.data.data.skillList ? ', ' : ''}${item.name}`;
+                    this.item.update({ "data.skillList": skillList }, {});
+                } else if (item.type === "trait") {
+                    const traitList = `${this.item.data.data.traitList}${this.item.data.data.traitList ? ', ' : ''}${item.name}`;
+                    this.item.update({ "data.traitList": traitList }, {});
+                }
+            }
+        }
     }
 }

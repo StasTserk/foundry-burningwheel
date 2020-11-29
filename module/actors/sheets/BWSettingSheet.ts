4903c7a6df4bf9c0aeae099dc68a6e87c0ba6aee
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-11-28 20:09:48 -0700

    Add setting sheet listeners
    Add edit, delete, and drag functionality to the setting sheet.
    Minor style improvements to make it match the book's setting page more
    closely.

--- module/actors/sheets/BWSettingSheet.ts
+++ module/actors/sheets/BWSettingSheet.ts
@@ -1,5 +1,7 @@
+import { BWItem } from "module/items/item.js";
 import { LifepathRootData } from "../../items/lifepath.js";
 import { BWSetting } from "../BWSetting.js";
+import * as helpers from "../../helpers.js";
 
 export class BWSettingSheet extends ActorSheet {
     get template(): string {
@@ -11,8 +13,38 @@ export class BWSettingSheet extends ActorSheet {
 
     getData(): BWSettingData {
         const data = super.getData() as BWSettingData;
-        data.lifepaths = data.actor.items as unknown as LifepathRootData[];
+        data.lifepaths = (data.actor.items as unknown as LifepathRootData[]).sort((a, b) => a.data.order - b.data.order);
         return data;
+    }
+
+    activateListeners(html: JQuery): void {
+        super.activateListeners(html);
+        html.find('.lifepath[draggable="true"]').on('dragstart', (e) => {
+            const actor = this.actor;
+            const item = actor.getOwnedItem(e.target.dataset.id || "") as BWItem;
+            const dragData: helpers.ItemDragData = {
+                actorId: actor.id,
+                id: item.id,
+                type: "Item",
+                data: item.data
+            };
+
+            if (e.originalEvent?.dataTransfer) {
+                e.originalEvent.dataTransfer.setData('text/plain', JSON.stringify(dragData));
+            }
+        });
+
+        html.find('i[data-action="delete"]').on('click', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            const id = e.target.dataset.id || "";
+            this.actor.deleteOwnedItem(id);
+        });
+
+        html.find('.lifepath').on('click', (e) => {
+            const id = e.currentTarget.dataset.id || "";
+            this.actor.getOwnedItem(id)?.sheet.render(true);
+        });
     }
 }
 

19cd652e8986adb9e873465705cd08b0f8bbfb53
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-12-03 16:36:33 -0700

    Add belief and instinct broadcasting

--- module/items/instinct.ts
+++ module/items/instinct.ts
@@ -1,7 +1,26 @@
+import { BWActor } from "../actors/BWActor.js";
 import { ArthaEarner, BWItem, BWItemData } from "./item.js";
 
 export class Instinct extends BWItem {
     data: BWItemData & { data: InstinctData };
+
+    async generateChatMessage(actor: BWActor): Promise<Entity> {
+        const data = {
+            title: this.name,
+            mainText: this.data.data.text,
+            extraData: [
+                {
+                    title: `Spent Artha`,
+                    text: `Fate: ${this.data.data.fateSpent || 0}; Persona: ${this.data.data.personaSpent || 0}; Deeds: ${this.data.data.deedsSpent || 0}`
+                }
+            ]
+        };
+        const html = await renderTemplate("systems/burningwheel/templates/chat/simple-broadcast.hbs", data);
+        return ChatMessage.create({
+            content: html,
+            speaker: ChatMessage.getSpeaker({actor})
+        });
+    }
 }
 
 interface InstinctData extends ArthaEarner {

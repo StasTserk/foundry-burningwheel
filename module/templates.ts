ad4a06ce30939446ea0e847eba2f6c1edb79ce53
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-12-04 12:37:48 -0700

    Fix styling on character sheet traits.
    Traits now properly utilize flex box and are more compact.

--- module/templates.ts
+++ module/templates.ts
@@ -11,6 +11,7 @@ export async function preloadHandlebarsTemplates(): Promise<void> {
       "systems/burningwheel/templates/parts/rollable-item.hbs",
       "systems/burningwheel/templates/parts/rollable-skill.hbs",
       "systems/burningwheel/templates/parts/spell.hbs",
+      "systems/burningwheel/templates/parts/trait.hbs",
       "systems/burningwheel/templates/parts/weapons.hbs",
       "systems/burningwheel/templates/parts/character-settings.hbs",
 

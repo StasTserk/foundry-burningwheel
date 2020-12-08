c5346cc20a9f4833bd9ca75b53f6d61c87e4ff5e
Stas Tserkovny <stas.tserkovny@gmail.com>
2020-12-07 17:52:49 -0700

    Simple registration and viewing of help dice.

---
+++ module/dialogs/buildHelpDialog.ts
@@ -0,0 +1,63 @@
+import { extractCheckboxValue, extractMiscObs, extractNumber } from "../rolls/rolls.js";
+import { BWActor, RollModifier } from "../actors/BWActor.js";
+import { difficultyGroup } from "../helpers.js";
+import { DifficultyDialog } from "./DifficultyDialog.js";
+import { ModifierDialog } from "./ModifierDialog.js";
+
+export async function buildHelpDialog({ exponent, path, skillId, actor, rollModifiers, useCustomDifficulty }: HelpDialogData): Promise<unknown> {
+    const data = {
+        exponent,
+        optionalObModifiers: rollModifiers,
+        useCustomDifficulty,
+    };
+    const content = await renderTemplate("systems/burningwheel/templates/dialogs/help-dialog.hbs", data);
+    return new Dialog({
+        title: "Add Helping Dice",
+        content: content,
+        buttons: {
+            help: {
+                label: "Help",
+                callback: (html: JQuery) => {
+                    registerHelpEntry({ html, path, skillId, actor, exponent});
+                }
+            }
+        }
+    }).render(true);
+}
+
+async function registerHelpEntry({ html, path, skillId, actor, exponent }: HelpEntryData): Promise<void> {
+    const useCustomDifficulty = extractCheckboxValue(html, "forceCustomDifficulty");
+    const difficultyDialog: DifficultyDialog = game.burningwheel.gmDifficulty;
+    const modifiers: ModifierDialog = game.burningwheel.modifiers;
+
+    const difficulty = useCustomDifficulty ? extractNumber(html, "difficulty") : difficultyDialog.difficulty;
+    const moreObs = extractMiscObs(html).sum;
+
+    const difficultySum = difficulty + moreObs;
+    const dg = difficultyGroup(exponent, difficultySum);
+    modifiers.addHelp({
+        dice: exponent,
+        skillId,
+        path,
+        difficulty: dg,
+        actor,
+        title: actor.name
+    });
+}
+
+export interface HelpDialogData {
+    exponent: number;
+    path?: string;
+    skillId?: string;
+    actor: BWActor;
+    rollModifiers?: RollModifier[];
+    useCustomDifficulty: boolean;
+}
+
+interface HelpEntryData {
+    html: JQuery;
+    path?: string;
+    skillId?: string;
+    actor: BWActor;
+    exponent: number;
+}

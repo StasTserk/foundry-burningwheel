9be4ffc2afef191fb0299a3a85918d006cd1db3a
Stas Tserkovny <stas.tserkovny@gmail.com>
2021-12-21 14:19:11 -0700

    Add migration task to convert some skills
    Offloads the double duty that was being pulled by skills being listed
    as training so they cost 2 points to open in burning.
    
    Skills marked as martial or military keep their "training" checkbox
    status, but other skills swap over to counting as magical.

---
+++ module/migration/task121.ts
@@ -0,0 +1,61 @@
+import { BWItem } from "../items/item";
+import { Skill } from "../items/skill";
+
+export async function task121(): Promise<void> {
+    const items: (Skill)[] = Array.from(game.items?.values() || []).filter((i: BWItem) => i.type === "skill") as (Skill)[];
+    const updateInfo = {};
+
+    for (const item of items) {
+        const updateData = updateItem(item, updateInfo);
+        if (Object.values(updateData).length) {
+            await item.update(updateData);
+        }
+    }
+
+    const actors: Actor[] = Array.from(game.actors?.values() || []);
+    for (const actor of actors) {
+        for (const ownedItem of Array.from(actor.items.values()).filter((i: BWItem) => i.type === "skill") as (Skill)[]) {
+            const updateData = updateItem(ownedItem, updateInfo);
+            if (Object.values(updateData).length) {
+                await ownedItem.update(updateData);
+            }
+        }
+    }
+
+    const packs = Array.from(game.packs?.values() || []);
+    for (const pack of packs) {
+        if (pack.documentName === "Item") {
+            const packItems = await pack.getDocuments();
+            for (const item of Array.from(packItems.values()).filter((i: BWItem) => i.type === "skill") as (Skill)[]) {
+                const updateData = updateItem(item, updateInfo);
+                if (Object.values(updateData).length) {
+                    await item.update(updateData);
+                }
+            }
+        }
+    }
+
+    const updatedTypes = Object.keys(updateInfo);
+    const parts: string[] = [];
+    for (const types of updatedTypes) {
+        parts.push(`${updateInfo[types]} ${types}s`);
+    }
+    const message = updatedTypes.length ? `Updated ${parts.join(", ")}.` : "No entities needed to be updated.";
+    ui.notifications?.notify(message, "info");
+}
+
+function updateItem(item: Skill, updateInfo: Record<string, number>): Record<string, string> {
+    const data = {};
+    if (item.data.data.training && ["military", "martial"].indexOf(item.data.data.skilltype) === -1) {
+        data["data.training"] = false;
+        data["data.magical"] = true;
+    }
+    if (Object.values(data).length) {
+        if (updateInfo[item.data.type]) {
+            updateInfo[item.data.type] ++;
+        } else {
+            updateInfo[item.data.type] = 1;
+        }
+    }
+    return data;
+}

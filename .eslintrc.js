158bb8a4aaee0d1095ba17d3b739e15594b7572d
Stas Tserkovny <stas.tserkovny@gmail.com>
2023-06-23 15:13:09 -0600

    Add prettier to project and build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2017": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": "tsconfig.json",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "ignorePatterns": [
-        "webpack.config.js",
-        ".eslintrc.js"
-    ],
-    "rules": {
-        "no-case-declarations": "off",
-        "@typescript-eslint/no-unused-vars": [
-            "error",
-            { "argsIgnorePattern": "^_" }
-        ],
-        "@typescript-eslint/consistent-type-assertions": "error",
-        "semi": "off",
-        "@typescript-eslint/semi": ["error"],
-        "no-console": ["error", { allow: ["warn", "error"] }]
-    }
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['webpack.config.js', '.eslintrc.js'],
+  rules: {
+    'no-case-declarations': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-assertions': 'error',
+    semi: 'off',
+    '@typescript-eslint/semi': ['error'],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+  },
 };

9a4306aa999423664341c300832ba85ff778a967
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-07-14 18:39:12 -0600

    Relationship select use

--- module/items/sheets/relationship-sheet.ts
+++ module/items/sheets/relationship-sheet.ts
@@ -1,11 +1,25 @@
-import { BWItemSheet } from './bwItemSheet';
+import { BWItemSheet, BWItemSheetData } from './bwItemSheet';
 
 export class RelationshipSheet extends BWItemSheet {
     static get defaultOptions(): BaseEntitySheet.Options {
         return super.defaultOptions;
     }
 
+    getData(): RelationshipSheetData {
+        const data = super.getData() as RelationshipSheetData;
+        data.influenceOptions = {
+            minor: 'BW.relationship.minor',
+            significant: 'BW.relationship.significant',
+            powerful: 'BW.relationship.powerful',
+        };
+        return data;
+    }
+
     get template(): string {
         return 'systems/burningwheel/templates/items/relationship.hbs';
     }
 }
+
+type RelationshipSheetData = BWItemSheetData & {
+    influenceOptions: Record<string, string>;
+};

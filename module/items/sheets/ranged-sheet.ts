795bbf71a905aff239655f6680f6a7eecd2d5fce
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-07-14 18:34:58 -0600

    Ranged weapon select usage

--- module/items/sheets/ranged-sheet.ts
+++ module/items/sheets/ranged-sheet.ts
@@ -1,4 +1,4 @@
-import { gearQualitySelect } from '../../constants';
+import { gearQualitySelect, shadesSelect } from '../../constants';
 import { BWItemSheet, BWItemSheetData } from './bwItemSheet';
 
 export class RangedWeaponSheet extends BWItemSheet {
@@ -9,10 +9,12 @@ export class RangedWeaponSheet extends BWItemSheet {
     getData(): RangedSheetData {
         const data = super.getData() as RangedSheetData;
         data.weaponQualities = gearQualitySelect;
+        data.shades = shadesSelect;
         return data;
     }
 }
 
 interface RangedSheetData extends BWItemSheetData {
     weaponQualities: { [index: string]: string };
+    shades: typeof shadesSelect;
 }

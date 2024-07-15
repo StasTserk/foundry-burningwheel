b1a1be6e5b13ed09c98167e88b2a4ab4c3579677
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-07-14 18:08:34 -0600

    Affiliation select usage

--- module/items/sheets/affiliation-sheet.ts
+++ module/items/sheets/affiliation-sheet.ts
@@ -1,7 +1,22 @@
-import { BWItemSheet } from './bwItemSheet';
+import { BWItemSheet, BWItemSheetData } from './bwItemSheet';
 
 export class AffiliationSheet extends BWItemSheet {
     get template(): string {
         return 'systems/burningwheel/templates/items/affiliation.hbs';
+    }
+
+    getData(): BWItemSheetData & {
+        affiliationOptions: Record<number, string>;
+    } {
+        const data = super.getData() as BWItemSheetData & {
+            affiliationOptions: Record<number, string>;
+        };
+        data.affiliationOptions = {
+            0: game.i18n.localize('BW.none'),
+            1: `1${game.i18n.localize('BW.diceAcronym')}`,
+            2: `3${game.i18n.localize('BW.diceAcronym')}`,
+            3: `3${game.i18n.localize('BW.diceAcronym')}`,
+        };
+        return data;
     }
 }

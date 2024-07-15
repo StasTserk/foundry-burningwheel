a207aec541bc579a7d055cf178ed6c8e797ae14d
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-07-14 18:42:01 -0600

    Rep select usage

--- module/items/sheets/reputation-sheet.ts
+++ module/items/sheets/reputation-sheet.ts
@@ -1,6 +1,16 @@
-import { BWItemSheet } from './bwItemSheet';
+import { BWItemSheet, BWItemSheetData } from './bwItemSheet';
 
 export class ReputationSheet extends BWItemSheet {
+    getData(): ReputationSheetData {
+        const data = super.getData() as ReputationSheetData;
+        data.diceOptions = {
+            0: game.i18n.localize('BW.none'),
+            1: `1${game.i18n.localize('BW.diceAcronym')}`,
+            2: `3${game.i18n.localize('BW.diceAcronym')}`,
+            3: `3${game.i18n.localize('BW.diceAcronym')}`,
+        };
+        return data;
+    }
     static get defaultOptions(): BaseEntitySheet.Options {
         return super.defaultOptions;
     }
@@ -9,3 +19,7 @@ export class ReputationSheet extends BWItemSheet {
         return 'systems/burningwheel/templates/items/reputation.hbs';
     }
 }
+
+type ReputationSheetData = BWItemSheetData & {
+    diceOptions: Record<string, string>;
+};

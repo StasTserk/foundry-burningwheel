75e5e08e7280f8083fe00516406f39d6d7b91471
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-07-29 00:43:19 -0600

    Weapon attacks

--- module/items/sheets/melee-sheet.ts
+++ module/items/sheets/melee-sheet.ts
@@ -40,14 +40,14 @@ export class MeleeWeaponSheet extends BWItemSheet {
                 weaponLength: 'Shortest',
                 weaponSpeed: '2',
             });
-            this.item.update({ 'data.attacks': attacks }, {});
+            this.item.update({ 'system.attacks': attacks }, {});
         });
         html.find('.fa-minus').on('click', (e: JQuery.ClickEvent) => {
             const target = e.target;
             const index = parseInt(target.dataset.index);
             const attacks = Object.values(this.item.system.attacks || []);
             attacks.splice(index, 1);
-            this.item.update({ 'data.attacks': attacks }, {});
+            this.item.update({ 'system.attacks': attacks }, {});
         });
     }
 }

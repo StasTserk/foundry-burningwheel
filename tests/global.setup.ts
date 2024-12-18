75376a24ca6d45b1d34c885ff974244dea12e2f2
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-12-18 11:36:25 -0700

    Add license init function

--- tests/global.setup.ts
+++ tests/global.setup.ts
@@ -7,15 +7,24 @@ if (!fs.existsSync('./foundryConfig.json')) {
     console.log('*** No config file found, creating new copy. ***');
     fs.writeFileSync(
         './foundryConfig.json',
-        JSON.stringify({ deployDest: './release' })
+        JSON.stringify({
+            deployDest: './release',
+            foundrySecret: "enter your license file's signature here",
+            foundryLicense: "enter your license file's license here",
+            foundryUsername: 'enter your foundry username here',
+            foundryPassword: 'enter your foundry password here',
+        })
     );
 } else {
     console.log('*** Found foundryConfig.json ***');
 }
 
-const config: { foundryUsername: string; foundryPassword: string } = JSON.parse(
-    fs.readFileSync('foundryConfig.json').toString()
-);
+const config: {
+    foundryUsername: string;
+    foundryPassword: string;
+    foundryLicense: string;
+    foundrySecret: string;
+} = JSON.parse(fs.readFileSync('foundryConfig.json').toString());
 
 async function authenticate() {
     return config;
@@ -26,8 +35,40 @@ async function maybeDownloadFoundry() {
     return auth;
 }
 
+async function maybeInitLicense() {
+    if (!fs.existsSync('./tests/license.json')) {
+        console.log('Generating license file');
+
+        const license = process.env.foundry_license ?? config.foundryLicense;
+        const signature = process.env.foundry_secret ?? config.foundrySecret;
+
+        if (
+            !license ||
+            !signature ||
+            license === "enter your license file's signature here" ||
+            signature === "enter your license file's signature here"
+        ) {
+            throw Error(
+                'Make sure to provide a valid license and signature in either foundryConfig.json or in your environment variables'
+            );
+        }
+
+        fs.writeFileSync(
+            './tests/license.json',
+            JSON.stringify({
+                host: 'foundry-host',
+                license: process.env.foundry_license ?? config.foundryLicense,
+                version: '11.293',
+                time: new Date(Date.now()).toISOString(),
+                signature: process.env.foundry_secret ?? config.foundrySecret,
+            })
+        );
+    }
+}
+
 export async function globalSetup(_config: FullConfig) {
     await maybeDownloadFoundry();
+    await maybeInitLicense();
 }
 
 export default globalSetup;

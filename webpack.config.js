6ab76dd96dd50d86926bf5a9549af59081e66caa
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-12-18 11:36:25 -0700

    Add license init function

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,13 @@ if (!fs.existsSync('./foundryConfig.json')) {
   console.log('*** No config file found, creating new copy. ***');
   fs.writeFileSync(
     './foundryConfig.json',
-    JSON.stringify({ deployDest: './release' })
+    JSON.stringify({
+      deployDest: './release',
+      foundrySecret: "enter your license file's signature here",
+      foundryLicense: "enter your license file's license here",
+      foundryUsername: 'enter your foundry username here',
+      foundryPassword: 'enter your foundry password here',
+    })
   );
 } else {
   console.log('*** Found foundryConfig.json ***');

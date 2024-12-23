668dc9788bbf7b68a876912589d06c4e7b97dc80
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-12-23 14:25:11 -0700

    Add some basic tests for data loading

--- tests/fixtures/bwFixture.ts
+++ tests/fixtures/bwFixture.ts
@@ -4,16 +4,17 @@ import {
     test as testLoggedOut,
     testAsGm as testLoggedIn,
 } from './gameFixture';
-import { expect } from 'playwright/test';
+import { expect, Locator } from 'playwright/test';
 
 type FixtureBase = typeof testLoggedOut & typeof testLoggedIn;
 
 type BwFixture = {
-    dowDialog: DoWDialogFixture;
+    dowDialog: DoWDialog;
+    fightDialog: FightDialog;
     rollDialog: RollDialog;
 };
 
-class DoWDialogFixture {
+class DoWDialog {
     constructor(
         private readonly page: Page,
         private readonly gamePage: GameFixture,
@@ -70,6 +71,81 @@ class DoWDialogFixture {
     }
 }
 
+class FightDialog {
+    constructor(
+        private readonly page: Page,
+        private readonly gamePage: GameFixture,
+        private readonly test: FixtureBase
+    ) {}
+
+    async OpenDialog() {
+        await this.gamePage.openTab('Combat Encounters');
+        await this.test.step('open fight dialog', async () => {
+            await this.page.getByRole('button', { name: /fight/i }).click();
+        });
+    }
+
+    expectVisibleAction({
+        fighter,
+        volley,
+        value,
+    }: {
+        fighter: number;
+        volley: number;
+        value: string | string[];
+    }) {
+        return expect(
+            this.page.getByLabel(`participant ${fighter} action ${volley}`),
+            {
+                message: `Expect p${fighter} v${volley} to be ${value}`,
+            }
+        ).toHaveText(value);
+    }
+
+    async expectHiddenAction({
+        fighter,
+        volley,
+    }: {
+        fighter: number;
+        volley: number;
+    }) {
+        return expect(
+            this.page
+                .getByLabel(`participant ${fighter} action ${volley}`)
+                .locator('i'),
+            {
+                message: `Expect p${fighter} v${volley} to be hidden`,
+            }
+        ).toBeVisible();
+    }
+
+    async expectScriptedAction({
+        fighter,
+        volley,
+        action,
+        value,
+    }: {
+        fighter: number;
+        volley: number;
+        action: number;
+        value: string;
+    }) {
+        return expect(
+            this.page
+                .getByLabel(`participant ${fighter} script ${volley}`)
+                .locator(`select[name="action${action}"]`)
+                .locator('option[selected]'),
+            {
+                message: `Expect p${fighter} v${volley} a${action} to be ${value}`,
+            }
+        ).toHaveText(value);
+    }
+
+    playerScript(player: number): Locator {
+        return this.page.getByLabel(`participant ${player} script 1`);
+    }
+}
+
 class RollDialog {
     constructor(private readonly page: Page) {}
 
@@ -91,7 +167,9 @@ class RollDialog {
 
 const extender: Parameters<typeof testLoggedIn.extend<BwFixture>>[0] = {
     dowDialog: async ({ page, gamePage }, use) =>
-        await use(new DoWDialogFixture(page, gamePage, test)),
+        await use(new DoWDialog(page, gamePage, test)),
+    fightDialog: async ({ page, gamePage }, use) =>
+        await use(new FightDialog(page, gamePage, test)),
     rollDialog: async ({ page }, use) => await use(new RollDialog(page)),
 };
 

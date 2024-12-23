d052ad83ebd2588fe59fc7993c2e22853c448a57
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-12-23 15:18:34 -0700

    Fix data loading

--- tests/fixtures/baseFixture.ts
+++ tests/fixtures/baseFixture.ts
@@ -10,23 +10,36 @@ export const baseFixture = baseTest.extend<TestFixtureBase>({
         async ({ page }, use) => {
             let foundryHost: StartedTestContainer | undefined;
             try {
-                console.log('Starting foundry host for tests');
+                console.log('[test-fixture] Starting foundry host for tests');
                 foundryHost = await foundryInstance();
                 const hostUrl = `http://${foundryHost.getHost()}:${foundryHost.getMappedPort(
                     30000
                 )}`;
-                console.log(`Started foundry at ${hostUrl}`);
+                console.log(`[test-fixture] Started foundry at ${hostUrl}`);
 
                 await page.goto(hostUrl);
                 // agree to eula
                 await page.getByLabel('I agree to these terms').click();
                 await page.getByRole('button', { name: /agree/i }).click();
 
+                page.on('console', (msg) => {
+                    if (msg.type() === 'error') {
+                        console.error(`[foundry-ui] ${msg.text()}`);
+                    } else if (msg.type() === 'warning') {
+                        console.warn(`[foundry-ui] ${msg.text()}`);
+                    } else {
+                        console.log(`[foundry-ui] ${msg.text()}`);
+                    }
+                });
+
                 await use(hostUrl);
             } catch (exn) {
-                console.error('Failed to start foundry host', exn);
+                console.error(
+                    '[test-fixture] Failed to start foundry host',
+                    exn
+                );
             } finally {
-                console.log(`Cleaning up after test`);
+                console.log(`[test-fixture] Cleaning up after test`);
                 foundryHost?.stop();
             }
         },

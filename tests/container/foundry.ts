3b7b30fec48ca4aa6228d2ec6be82849a173fcb5
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-12-24 16:43:39 -0700

    Test parallel execution

--- tests/container/foundry.ts
+++ tests/container/foundry.ts
@@ -1,5 +1,19 @@
 /* eslint-disable no-console */
 import { GenericContainer } from 'testcontainers';
+
+export function generateHostPort() {
+    if (!process.env.TEST_PARALLEL_INDEX) {
+        throw new Error('TEST_PARALLEL_INDEX was not set');
+    }
+    const parallelIndex = Number.parseInt(process.env.TEST_PARALLEL_INDEX);
+    if (Number.isNaN(parallelIndex)) {
+        throw new Error('TEST_PARALLEL_INDEX was not a valid integer');
+    }
+    let shard = Number.parseInt(process.env.CIRCLE_NODE_INDEX || '');
+    shard = Number.isNaN(shard) ? 10 : shard * 10;
+    return 30000 + parallelIndex + shard;
+}
+
 export async function foundryInstance() {
     console.log(
         `[test-fixture] Grabbing image from ${process.cwd()}/tests/container/Dockerfile`
@@ -12,7 +26,7 @@ export async function foundryInstance() {
     )
         .withExposedPorts({
             container: 30000,
-            host: 30000,
+            host: generateHostPort(),
         })
         .withNetworkAliases('foundry-host')
         .withLogConsumer((stream) => {

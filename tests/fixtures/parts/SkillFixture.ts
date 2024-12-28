c9136f036f350b5cb4fbae501dc9259990ac5b9b
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-12-27 18:19:24 -0700

    Add item partial fixture stubs

---
+++ tests/fixtures/parts/SkillFixture.ts
@@ -0,0 +1,10 @@
+import { Page } from 'playwright/test';
+import { FixtureBase } from '../bwFixture';
+import { GameFixture } from '../gameFixture';
+import { BaseItemFixture } from './BaseItemFixture';
+
+export class SkillFixture extends BaseItemFixture {
+    constructor(page: Page, gamePage: GameFixture, test: FixtureBase) {
+        super(page, gamePage, test);
+    }
+}

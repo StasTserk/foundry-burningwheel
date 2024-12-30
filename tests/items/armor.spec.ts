a44295a4528720735c808da2457da52f2a376b55
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-12-29 17:17:14 -0700

    Refactor armor test to use item fixture

--- tests/items/armor.spec.ts
+++ tests/items/armor.spec.ts
@@ -12,34 +12,38 @@ test('loads sheet data', async ({ items: { armor } }) => {
         'A made up item with every field different from the default'
     );
 
-    await expect(sheet.hasHelm).toBeChecked();
-    await expect(sheet.hasTorso).toBeChecked();
-    await expect(sheet.hasLeftArm).toBeChecked();
-    await expect(sheet.hasRightArm).toBeChecked();
-    await expect(sheet.hasLeftLeg).toBeChecked();
-    await expect(sheet.hasRightLeg).toBeChecked();
-    await expect(sheet.hasShield).toBeChecked();
+    await expect(sheet.getLabeledField('has helm')).toBeChecked();
+    await expect(sheet.getLabeledField('has torso')).toBeChecked();
+    await expect(sheet.getLabeledField('has left arm')).toBeChecked();
+    await expect(sheet.getLabeledField('has shield')).toBeChecked();
 
-    await expect(sheet.rpCost).toHaveValue('44');
-    await expect(sheet.quality).toHaveValue('run of the mill');
-    await expect(sheet.helmDamage).toHaveValue('1');
-    await expect(sheet.torsoDamage).toHaveValue('3');
+    await expect(sheet.getLabeledField('resource point cost')).toHaveValue(
+        '44'
+    );
+    await expect(sheet.getLabeledField('armor quality')).toHaveValue(
+        'run of the mill'
+    );
+    await expect(sheet.getLabeledField('fatigue penalty')).toHaveValue('4');
+    await expect(sheet.getLabeledField('helm damage')).toHaveValue('1');
+    await expect(sheet.getLabeledField('left arm damage')).toHaveValue('7');
 });
 
 test('can be edited, remembers changes', async ({ items: { armor } }) => {
     const sheet = await armor.openDialog('Modified Armor');
     await test.step('change some fields', async () => {
         await sheet.setDescription('updated description');
-        await sheet.toggleHasHelm();
-        await sheet.torsoDamage.fill('33');
+        await sheet.togglePillCheckbox('has left arm', true);
+        await sheet.setLabeledField('torso damage', '33');
         await sheet.close();
     });
 
     await test.step('check these changes got saved', async () => {
         await sheet.open();
-        await expect(sheet.hasHelm).not.toBeChecked();
-        await expect(sheet.helmDamage).not.toBeVisible();
-        await expect(sheet.torsoDamage).toHaveValue('33');
+        await expect(sheet.getLabeledField('has left arm')).not.toBeChecked();
+        await expect(
+            sheet.getLabeledField('left arm damage')
+        ).not.toBeVisible();
+        await expect(sheet.getLabeledField('torso damage')).toHaveValue('33');
         await expect(sheet.description).toHaveValue('updated description');
     });
 });

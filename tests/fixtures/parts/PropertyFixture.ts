b19786d069e76b3762764f4c21b4d0fb3999aea0
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-12-29 18:22:41 -0700

    Add property test

--- tests/fixtures/parts/PropertyFixture.ts
+++ tests/fixtures/parts/PropertyFixture.ts
@@ -1,10 +1,28 @@
 import { Page } from 'playwright/test';
 import { FixtureBase } from '../bwFixture';
 import { GameFixture } from '../gameFixture';
-import { BaseItemFixture } from './BaseItemFixture';
+import { BaseItemDialog, BaseItemFixture } from './BaseItemFixture';
+import { SeededItems } from '../SeededData';
 
+type LabelFields = 'Resource Point Cost';
+type CheckBoxFields = 'Workshop';
+type SelectFields = never;
+class PropertyDialog extends BaseItemDialog<
+    LabelFields,
+    CheckBoxFields,
+    SelectFields
+> {
+    constructor(fixture: BaseItemFixture, name: SeededItems) {
+        super(fixture, name);
+    }
+}
 export class PropertyFixture extends BaseItemFixture {
     constructor(page: Page, gamePage: GameFixture, test: FixtureBase) {
         super(page, gamePage, test);
+    }
+
+    async openDialog(name: SeededItems<'property'>) {
+        await super.open(name);
+        return new PropertyDialog(this, name);
     }
 }

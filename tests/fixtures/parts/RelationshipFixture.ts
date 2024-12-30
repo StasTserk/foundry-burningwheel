f152ac7913a26b716091fe09a599449640526946
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-12-29 19:27:33 -0700

    Test reputations and relationships

--- tests/fixtures/parts/RelationshipFixture.ts
+++ tests/fixtures/parts/RelationshipFixture.ts
@@ -1,10 +1,35 @@
 import { Page } from 'playwright/test';
 import { FixtureBase } from '../bwFixture';
 import { GameFixture } from '../gameFixture';
-import { BaseItemFixture } from './BaseItemFixture';
+import { BaseItemDialog, BaseItemFixture } from './BaseItemFixture';
+import { SeededItems } from '../SeededData';
 
+type LabelFields = never;
+type CheckBoxFields =
+    | 'Forbidden'
+    | 'Immediate Family'
+    | 'Other Family'
+    | 'Romantic'
+    | 'Hateful/Rival'
+    | 'Building'
+    | 'Enmity Clause';
+type SelectFields = 'Influence';
+class RelationshipDialog extends BaseItemDialog<
+    LabelFields,
+    CheckBoxFields,
+    SelectFields
+> {
+    constructor(fixture: BaseItemFixture, name: SeededItems) {
+        super(fixture, name);
+    }
+}
 export class RelationshipFixture extends BaseItemFixture {
     constructor(page: Page, gamePage: GameFixture, test: FixtureBase) {
         super(page, gamePage, test);
+    }
+
+    async openDialog(name: SeededItems<'relationship'>) {
+        await super.open(name);
+        return new RelationshipDialog(this, name);
     }
 }
--- tests/items/reputation.spec.ts
+++ tests/items/reputation.spec.ts
@@ -1,6 +1,30 @@
+import { expect } from 'playwright/test';
 import { testAsGm as test } from '../fixtures/bwFixture';
 
 test('can be created, opens sheet', async ({ gamePage, items }) => {
     await gamePage.createItem('Test Reputation', 'reputation');
     await items.rep.expectOpened('Test Reputation');
+});
+
+test('loads sheet data', async ({ items: { rep } }) => {
+    const dialog = await rep.openDialog('Modified Reputation');
+
+    await expect(dialog.getLabeledField('Infamous')).toBeChecked();
+    await expect(dialog.getLabeledField('Dice')).toHaveValue('3');
+    await expect(dialog.description).toHaveValue('A modified text description');
+});
+
+test('can be modified', async ({ items: { rep } }) => {
+    const dialog = await rep.openDialog('Modified Reputation');
+    await test.step('update the sheet data', async () => {
+        await dialog.togglePillCheckbox('Infamous');
+        await dialog.selectOption('Dice', '2');
+        await dialog.close();
+    });
+
+    await test.step('check changes got persisted', async () => {
+        await dialog.open();
+        await expect(dialog.getLabeledField('Infamous')).not.toBeChecked();
+        await expect(dialog.getLabeledField('Dice')).toHaveValue('2');
+    });
 });

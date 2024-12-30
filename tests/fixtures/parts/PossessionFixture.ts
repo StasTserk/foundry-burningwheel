846afef42d59b7d3ab56e82f9b94fc894ac7899f
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-12-29 18:17:30 -0700

    Test possession sheet

--- tests/fixtures/parts/PossessionFixture.ts
+++ tests/fixtures/parts/PossessionFixture.ts
@@ -1,10 +1,28 @@
 import { Page } from 'playwright/test';
 import { FixtureBase } from '../bwFixture';
 import { GameFixture } from '../gameFixture';
-import { BaseItemFixture } from './BaseItemFixture';
+import { BaseItemDialog, BaseItemFixture } from './BaseItemFixture';
+import { SeededItems } from '../SeededData';
 
+type LabelFields = 'Resource Point Cost';
+type CheckBoxFields = 'Toolkit' | 'Expended';
+type SelectFields = never;
+class PossessionDialog extends BaseItemDialog<
+    LabelFields,
+    CheckBoxFields,
+    SelectFields
+> {
+    constructor(fixture: BaseItemFixture, name: SeededItems) {
+        super(fixture, name);
+    }
+}
 export class PossessionFixture extends BaseItemFixture {
     constructor(page: Page, gamePage: GameFixture, test: FixtureBase) {
         super(page, gamePage, test);
+    }
+
+    async openDialog(name: SeededItems<'possession'>) {
+        await super.open(name);
+        return new PossessionDialog(this, name);
     }
 }

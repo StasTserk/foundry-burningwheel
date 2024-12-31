8993c872cdab90d07ec6323c72a3d24e30e04541
Stas Tserkovny <stas.tserkovny@gmail.com>
2024-12-30 20:24:05 -0700

    Test running parallel workers in tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : 2,
+    workers: process.env.CI ? 2 : 2,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
         ['html', { outputFolder: 'results' }],

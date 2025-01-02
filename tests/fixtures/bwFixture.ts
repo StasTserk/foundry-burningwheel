a675c81c6c44fda8e7240fe8748095e10f8ec0ab
Stas Tserkovny <stas.tserkovny@gmail.com>
2025-01-01 17:06:50 -0700

    Roll testing initiated

--- tests/fixtures/bwFixture.ts
+++ tests/fixtures/bwFixture.ts
@@ -3,6 +3,7 @@ import { AffiliationFixture } from './parts/AffiliationFixture';
 import { ArmorFixture } from './parts/ArmorFixture';
 import { BeliefFixture } from './parts/BeliefFixture';
 import { CharacterFixture } from './parts/CharacterFixture';
+import { ChatFixture } from './parts/ChatFixture';
 import { DoWDialog } from './parts/DoWDialog';
 import { FightDialog } from './parts/FightDialog';
 import { InstinctFixture } from './parts/InstinctFixture';
@@ -14,7 +15,7 @@ import { RangeAndCoverDialog } from './parts/RangeAndCoverDialog';
 import { RangedWeaponFixture } from './parts/RangedWeaponFixture';
 import { RelationshipFixture } from './parts/RelationshipFixture';
 import { ReputationFixture } from './parts/ReputationFixture';
-import { RollDialog } from './parts/RollDialog';
+import { RollFixture } from './parts/RollDialog';
 import { SkillFixture } from './parts/SkillFixture';
 import { SpellFixture } from './parts/SpellFixture';
 import { TraitFixture } from './parts/TraitFixture';
@@ -25,8 +26,9 @@ type BwFixture = {
     dowDialog: DoWDialog;
     fightDialog: FightDialog;
     rncDialog: RangeAndCoverDialog;
-    rollDialog: RollDialog;
+    rollDialog: RollFixture;
     char: CharacterFixture;
+    chat: ChatFixture;
     items: {
         aff: AffiliationFixture;
         armor: ArmorFixture;
@@ -50,11 +52,13 @@ const extender: Parameters<typeof testLoggedIn.extend<BwFixture>>[0] = {
         await use(new DoWDialog(page, gamePage, test)),
     fightDialog: async ({ page, gamePage }, use) =>
         await use(new FightDialog(page, gamePage, test)),
-    rollDialog: async ({ page }, use) => await use(new RollDialog(page)),
+    rollDialog: async ({ page }, use) => await use(new RollFixture(page)),
     rncDialog: async ({ page, gamePage }, use) =>
         await use(new RangeAndCoverDialog(page, gamePage, test)),
     char: async ({ page, gamePage }, use) =>
         await use(new CharacterFixture(page, gamePage, test)),
+    chat: async ({ page, gamePage }, use) =>
+        await use(new ChatFixture(page, gamePage)),
     items: async ({ page, gamePage }, use) =>
         await use({
             aff: new AffiliationFixture(page, gamePage, test),
--- tests/fixtures/parts/RollDialog.ts
+++ tests/fixtures/parts/RollDialog.ts
@@ -1,6 +1,55 @@
-import { Page, expect } from 'playwright/test';
+import { Locator, Page, expect } from 'playwright/test';
 
 export class RollDialog {
+    constructor(readonly locator: Locator) {}
+
+    expectOpened() {
+        return expect(this.locator).toBeVisible();
+    }
+
+    async close() {
+        this.locator.getByText(/close/i).click();
+        await expect(this.locator).not.toBeVisible();
+    }
+
+    get woundPenalty() {
+        return this.locator.getByLabel('wound penalty');
+    }
+
+    get exponent() {
+        return this.locator.getByLabel('exponent dice');
+    }
+    get bonusDice() {
+        return this.locator.getByLabel('bonus dice');
+    }
+    setBonusDice(value: number) {
+        return this.bonusDice.fill('' + value);
+    }
+
+    fork(name: string) {
+        return this.locator
+            .getByLabel('fork dice')
+            .getByLabel(RegExp(name, 'i'));
+    }
+
+    roll() {
+        return this.locator
+            .getByRole('button', { name: 'Roll', exact: true })
+            .click();
+    }
+
+    static getDialog(page: Page, name: string) {
+        return new RollDialog(
+            page.locator('div.window-app.dialog').filter({
+                has: page
+                    .locator('h4')
+                    .filter({ hasText: new RegExp(name, 'i') }),
+            })
+        );
+    }
+}
+
+export class RollFixture {
     constructor(private readonly page: Page) {}
 
     async expectOpened(skill: string) {

a55336b82c44b79bf1ea7eb88c8bc64e7449297a
Stas Tserkovny <stas.tserkovny@gmail.com>
2026-07-18 19:31:25 -0600

    Update version to 1.7.1

--- tests/setup/setup.mjs
+++ tests/setup/setup.mjs
@@ -18,7 +18,7 @@ if (!fs.existsSync('./foundryConfig.json')) {
     console.log('*** Found foundryConfig.json ***');
 }
 
-const version = process.argv.length === 3 ? process.argv[2] : '360';
+const version = process.argv.length === 3 ? process.argv[2] : '365';
 
 const config = JSON.parse(fs.readFileSync('foundryConfig.json').toString());
 
